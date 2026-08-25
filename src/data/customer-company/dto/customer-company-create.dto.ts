968fd44bfa94c40ce4b95645489794a96f536610
Aziz2k <kuchkarov.aziz.08@gmail.com>
2026-08-25 08:41:13 +0000

    fix: make company_district optional in CreateCustomerCompanyDto (existing frontend doesn't send it yet)

--- src/data/customer-company/dto/customer-company-create.dto.ts
+++ src/data/customer-company/dto/customer-company-create.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CreateCustomerCompanyDto {
   @IsString()
@@ -14,6 +14,7 @@ export class CreateCustomerCompanyDto {
   @IsString()
   company_region: string;
   @IsString()
+  @IsOptional()
   company_district: string;
   @IsString()
   company_email: string;
