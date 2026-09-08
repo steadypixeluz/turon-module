5f2b4c373818d5f408c34a9e80c0420e25aec88c
Aziz2k <kuchkarov.aziz.08@gmail.com>
2026-09-08 12:22:18 +0500

    add enum type to chat

--- dist/data/chats/chat.schemas.js
+++ dist/data/chats/chat.schemas.js
@@ -60,7 +60,7 @@ __decorate([
     (0, mongoose_1.Prop)({
         type: String,
         required: true,
-        enum: ['cargos', 'vehicle'],
+        enum: ['cargos', 'vehicle', 'product'],
         index: true,
     }),
     __metadata("design:type", String)
